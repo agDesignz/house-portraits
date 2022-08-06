e1d473586a8d9e0b3a5a4d48447901852ead59a3
mnartstuff <alexgeerartist@gmail.com>
2022-08-05 22:55:36 -0500

    JS to stop modal scrolling

---
+++ scripts/modal.js
@@ -0,0 +1,19 @@
+(function() {
+  "use strict";
+
+  //  https://css-tricks.com/prevent-page-scrolling-when-a-modal-is-open/
+
+  const body = document.querySelector("body");
+  const checkBox = document.querySelector("#nav-toggle");
+  const checkIt = function() {
+    if (this.checked) {
+      body.classList.add("modal-open");
+      body.style.top = `-${window.scrollY}px`;
+    } else {
+      body.classList.remove("modal-open");
+      body.style.top = '';
+      window.scrollTo(0, parseInt(scrollY || '0') * -1);
+    }
+  }
+  checkBox.addEventListener("change", checkIt);
+}());
