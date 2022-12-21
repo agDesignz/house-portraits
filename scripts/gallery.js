4107038b48a2ee1edcf8c5aef200b2608ce3b797
Alex <alexgeorgegeer@gmail.com>
2022-12-20 18:17:06 -0600

    updated gallery JS and grid layout

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,36 +1,54 @@
 (() => {
     'use strict';
 
-    const lightbox = document.createElement('div');
-    lightbox.className = 'lightbox';
-    document.body.appendChild(lightbox);
+    const galContainer = document.querySelector('.gallery__container');
+    const lightbox = document.querySelector('.lightbox');
+    const lightboxContainer = document.querySelector('.lightbox__container');
+    const btnCloseLight = document.querySelector('.lightbox__close');
 
-    [...document.querySelectorAll('.gallery__img')].forEach(img => {
-        img.addEventListener('click', (e) => {
-            document.body.style.overflow = 'hidden';
-            console.log(e.target.src);
-            lightbox.classList.add('active');
-            const lightImg = document.createElement('img');
-            lightImg.classList.add('lightbox__img');
-            lightImg.classList.add('up');
-            lightImg.src = e.target.src.replace('gal', 'print');
-            while (lightbox.firstChild) {
-                lightbox.removeChild(lightbox.firstChild);
-            }
-            lightbox.appendChild(lightImg);
-        });
-    });
+    const lightShow = function (e) {
+        const clicked = e.target;
+        if(!clicked) return;
+        document.body.style.overflow = 'hidden';
+        if (lightboxContainer.classList.contains('out')) lightboxContainer.classList.remove('out');   
+        lightbox.classList.remove('lightbox--hidden');
+        const lightImg = document.createElement('img');
+        lightImg.classList.add('lightbox__img');
+        lightImg.src = clicked.src.replace('gal', 'print');
+        if (lightboxContainer.querySelector('img')) {
+            lightboxContainer.removeChild(lightbox.querySelector('img'))
+        }
+        lightboxContainer.appendChild(lightImg);
 
-    lightbox.addEventListener('click', (e) => {
-        if (e.target !== e.currentTarget)
-            return;
+    };
+
+    const lightsOut = () => {
         const lightImg = document.querySelector('.lightbox__img');
-        lightImg.classList.add('out');      
+        lightboxContainer.classList.add('out');      
         setTimeout(() => {
             lightImg.style.display = 'none';
-            lightbox.classList.remove('active');
+            lightbox.classList.add('lightbox--hidden');
             document.body.style.overflow = 'auto';
+            lightboxContainer.removeChild(lightImg);
         }, 100); 
+    };
+
+    galContainer.addEventListener('click', lightShow);
+
+    lightbox.addEventListener('click', function (e) {
+        if (e.target !== e.currentTarget)
+        return;
+        lightsOut();
+    });
+    btnCloseLight.addEventListener('click', function (e) {
+        if (e.target !== e.currentTarget)
+        return;
+        lightsOut();
+    });
+    document.addEventListener('keydown', function (e) {
+        if (e.key === 'Escape' && !lightbox.classList.contains('lightbox--hidden')) {
+            lightsOut();
+        }
     });
 
 })();
